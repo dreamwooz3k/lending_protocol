12c24299c020c30472b9eadd3c5e5c9e6272e274
dreamwooz3k <113083338+dreamwooz3k@users.noreply.github.com>
2022-10-03 15:00:27 +0900

    Update dreamoracle.sol

--- src/dreamoracle.sol
+++ src/dreamoracle.sol
@@ -1,19 +1,23 @@
 pragma solidity ^0.8.13;
  
-contract DreamOracle {
+contract DreamOracle 
+{
    address public operator;
    mapping(address=>uint256) prices;
  
-   constructor() {
+   constructor() 
+   {
        operator = msg.sender;
    }
  
-   function getPrice(address token) external view returns (uint256) {
+   function getPrice(address token) external view returns (uint256) 
+   {
        require(prices[token] != 0, "the price cannot be zero");
        return prices[token];
    }
  
-   function setPrice(address token, uint256 price) external {
+   function setPrice(address token, uint256 price) external // Pricing any token in eth
+   {  
        require(msg.sender == operator, "only operator can set the price");
        prices[token] = price;
    }
