53003970e3bab97aaa6d94742d2cbdbf0208cc13
dreamwooz3k <113083338+dreamwooz3k@users.noreply.github.com>
2022-10-05 20:41:50 +0900

    patch_4
    patch_4

--- test/lending.t.sol
+++ test/lending.t.sol
@@ -39,10 +39,11 @@ contract Lending_test is Test
         dreamoracle = new DreamOracle();
         lending = new Lending(address(usdc), address(dreamoracle));
 
-        dreamoracle.setPrice(address(usdc), 1);
-        dreamoracle.setPrice(address(0), 200);
+        dreamoracle.setPrice(address(usdc), 1 ether);
+        dreamoracle.setPrice(address(0), 200 ether);
         usdc.mint(address(lending), 100 ether);
-        usdc.mint(address(this), 100 ether);
+        usdc.mint(address(this), type(uint224).max-1);
+        usdc.approve(address(lending), type(uint224).max-1);
 
         alice = address(0xa);
         vm.deal(alice, 100 ether);
@@ -66,6 +67,7 @@ contract Lending_test is Test
         vm.prank(alice);
         address(lending).call{value: 10 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 10 ether));
         assertEq(lending.guarantee_balanceOf(alice), 10 ether);
+        console.log(lending.atoken_balanceOf(alice));
 
         vm.expectRevert("Check that the deposit call was successful");
         lending.deposit(address(1), 123);
@@ -75,10 +77,10 @@ contract Lending_test is Test
     function testborrow() public
     {
         vm.startPrank(bob);
-        address(lending).call{value: 100 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 100 ether));
-        lending.borrow(address(usdc), 10000 ether);
+        address(lending).call{value: 1 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 1 ether));
+        lending.borrow(address(usdc), 100 ether);
         assertEq(lending.guarantee_balanceOf(bob), 0);
-        assertEq(usdc.balanceOf(bob), 100 ether + 10000);
+        assertEq(usdc.balanceOf(bob), 100 ether + 100 ether);
     }
 
     function testwithdraw() public
@@ -99,34 +101,38 @@ contract Lending_test is Test
     function testrepay() public
     {
         vm.startPrank(bob);
-        address(lending).call{value: 100 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 100 ether));
-        lending.borrow(address(usdc), 10000 ether);
-        lending.repay(address(usdc), 10000 ether);
+        address(lending).call{value: 1 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 1 ether));
+        lending.borrow(address(usdc), 100 ether);
+        lending.repay(address(usdc), 100 ether);
         assertEq(bob.balance, 100 ether);
     }
 
     function testliquidate() public
     {
         vm.startPrank(bob);
-        address(lending).call{value: 100 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 100 ether));
-        lending.borrow(address(usdc), 10000 ether);
+        address(lending).call{value: 1 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 1 ether));
+        lending.borrow(address(usdc), 100 ether);
         vm.stopPrank();
         
-        usdc.approve(address(lending), 10000);
+        usdc.approve(address(lending), 150 ether);
         dreamoracle.setPrice(address(usdc), 1.5 ether);
-        lending.liquidate(bob, address(usdc), 10000 ether);
+        lending.liquidate(bob, address(usdc), 90 ether);
+
+        assertEq(lending.lock_guarantee_balanceOf(bob), 325000000000000000);
     }
 
     function testfee() public
     {
+        uint256 before_balance = address(this).balance;
         vm.startPrank(bob);
-        address(lending).call{value: 100 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 100 ether));
-        lending.borrow(address(usdc), 10000 ether);
+        address(lending).call{value: 1 ether}(abi.encodeWithSignature("deposit(address,uint256)", address(0), 1 ether));
+        lending.borrow(address(usdc), 100 ether);
         uint256 t = block.timestamp;
         vm.stopPrank();
-        vm.warp(t + 3 days);
+        vm.warp(t + 500 days);
         lending.update_fee(bob);
-        console.log(lending.loan_balanceOf(bob));
+        lending.liquidate(bob, address(usdc), 64830941641303877091);
+        assertEq(before_balance, address(this).balance);
     }
 
     receive() payable external
